25d46e4e1fa803373407c9d3d3b01aff94f5255c
shuukasei <shuukasei@gmail.com>
2015-08-30 11:35:27 +0900

    chaged the file name

---
+++ app/src/main/java/com/enpit/mykt/Activity/daytimeMainActivity.java
@@ -0,0 +1,57 @@
+package com.enpit.mykt.Activity;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.enpit.mykt.Fragment.ScheduleToolsFragment;
+import com.enpit.mykt.Fragment.TimeFragment;
+import com.enpit.mykt.R;
+
+
+public class daytimeMainActivity extends Activity  implements ScheduleToolsFragment.OnFragmentInteractionListener,TimeFragment.OnFragmentInteractionListener {
+    ScheduleToolsFragment scheduleToolsFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.daytime_activity_main);
+        scheduleToolsFragment=(ScheduleToolsFragment)getFragmentManager().findFragmentById(R.id.fragment1);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
+    }
+}
